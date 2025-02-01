47c7a19e3cfde8e0e540de12f693729d4b5e2178
mjehra <virtualsteve786@gmail.com>
2025-02-01 22:36:19 +0530

    Update Setup.py

--- Setup.py
+++ Setup.py
@@ -36,7 +36,7 @@ def khan():
     
     baglantilar = {
         "1": "https://raw.githubusercontent.com/mjehra/Gmail-v1/9f921daaa75b8d24c40d80e114dc590f994fa9b8/GMAIL.py",
-        "2": "https://raw.githubusercontent.com/mjehra/G-h/5bc9585ab71107f7dfd1fea21b713096f41d8058/LAST%20JACK.py",
+        "2": "https://github.com/mjehra/G-h/blob/16e8d525d977ebf8f38acba760e8dc89dbe38d64/LAST%20JACK.py",
         "3": "https://raw.githubusercontent.com/mjehra/Aol-/02a6e9c10f76fd4572eb9565a97a9149ff27512a/RANDOM%20AOLS.py",
         "4":"https://raw.githubusercontent.com/mjehra/Crunchy/a6047f6ba5edecbb72e87a6c0892785d25db9bdd/CRUNCHYROLLS.py",
         "5":"https://raw.githubusercontent.com/mjehra/Fb-tool-/1b77f4188743fd83005919a98b46efba74d5c365/FB.py"
